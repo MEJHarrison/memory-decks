5980f5834332f58cfd2ce7b15e86f95ad2e885ab
MEJHarrison <mejharrison@hotmail.com>
2025-08-28 12:38:39 -0700

    Updated name

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -16,7 +16,9 @@ export default function MainMenu() {
 
     return (
         <div className="flex min-h-screen flex-col items-center gap-12 p-8">
-            <h1 className="mt-8 text-5xl font-bold tracking-wide text-emerald-800 drop-shadow md:mt-12">My Quiz App</h1>
+            <h1 className="mt-8 text-5xl font-bold tracking-wide text-emerald-800 drop-shadow md:mt-12">
+                Memory Decks
+            </h1>
 
             <div className="mt-12 flex flex-col items-center gap-8 md:mt-16 md:gap-12">
                 <Button onClick={startQuiz}>Start Quiz</Button>
