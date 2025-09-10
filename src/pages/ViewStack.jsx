3ce10fcc48f8fa200ae9c6440e7e38a5d90f775a
MEJHarrison <mejharrison@hotmail.com>
2025-09-10 12:33:16 -0700

    Moved Main Menu button on View Stack screen

--- src/pages/ViewStack.jsx
+++ src/pages/ViewStack.jsx
@@ -16,6 +16,10 @@ export default function FullStack() {
 
     return (
         <div>
+            <div className="m-16 flex flex-col items-center gap-12">
+                <Button onClick={() => navigate('/')}>Main Menu</Button>
+            </div>
+
             <div className="flex flex-wrap justify-center gap-2 p-2">
                 {deck.cards.map((card) => (
                     <div key={card.position} className="mb-4 text-center">
@@ -23,10 +27,6 @@ export default function FullStack() {
                         <h2 className="text-lg font-bold sm:text-3xl">{card.position}</h2>
                     </div>
                 ))}
-            </div>
-
-            <div className="m-16 flex flex-col items-center gap-12">
-                <Button onClick={() => navigate('/')}>Main Menu</Button>
             </div>
         </div>
     );
